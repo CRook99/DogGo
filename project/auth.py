2504c90004ecb8c07c013e252dcda153fb808c51
CRook99 <cjrook43@gmail.com>
2021-10-05 20:34:36 +0100

    WIP Phase 1

--- project/auth.py
+++ project/auth.py
@@ -3,9 +3,10 @@ from werkzeug.security import check_password_hash, generate_password_hash
 from database import get_db
 import sqlite3
 import os
+import re
+
 app = Flask(__name__)
 app.config.from_mapping(DATABASE = os.path.join(app.instance_path, 'schema.sql'))
-
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -24,20 +25,46 @@ def login():
 
     return render_template('auth/login.html', title=title)
 
+
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == "POST":
         email = request.form.get("email")
         telephoneNo = request.form.get("telephoneNo")
         password = request.form.get("password")
+        confirmPassword = request.form.get("confirmPassword")
         if request.form.get("login_button") == "login":
-            con = sqlite3.connect('database.db')
-            con.execute("INSERT INTO user (email, telephoneNo, password) VALUES (?, ?, ?)", (email, telephoneNo, generate_password_hash(password)))
+            if validateEmail(email):
+                con = sqlite3.connect('database.db')
+                con.execute("INSERT INTO user (email, telephoneNo, password) VALUES (?, ?, ?)", (email, telephoneNo, generate_password_hash(password)))
+            else:
+                pass
     return render_template('auth/register.html')
+
 
 @app.route('/')
 def index():
+    #return render_template('auth/index.html')
     return redirect(url_for('login'))
 
+
+def validateEmail(email):
+    return not (re.search(r"[^@]+@[^@]+\.[^@]+", email) == None) # Regex expression to check whether submitted email follows conventions
+
+
+def validatePassword(password):
+    return not (re.search(r"/(.*[A-Z].*)(.*\d.*)(^.{8,32})/", password) == None) # TODO fix
+
+
+def matchPasswords(password, confirmPassword):
+    return password == confirmPassword # Returns whether the first password and the confirmation password match
+
+
 if __name__ == '__main__':
+    print(validateEmail("test@test.com"))
+    print(validatePassword("Short1"))
+    print(validatePassword("Longlonglonglonglonglonglonglong1"))
+    print(validatePassword("capitalletter1"))
+    print(validatePassword("Nonumber"))
+    print(validatePassword("Validpass1"))
     app.run(debug = True)
